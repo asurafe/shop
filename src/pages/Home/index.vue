073efe22d2fdb7a7f853c847a339257c8c6be6b9
asurafe <onelana@163.com>
2023-01-29 16:43:01 +0800

    feat:home页UI样式

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
     <TypeNav />
+    <ListContainer />
+    <Recommend />
+    <Rank />
+    <Like />
+    <Floor />
+    <Brand />
   </div>
 </template>
 
 <script>
-export default {};
+import ListContainer from "@/pages/Home/ListContainer"
+import Recommend from "@/pages/Home/Recommend"
+import Rank from "@/pages/Home/Rank"
+import Like from "@/pages/Home/Like"
+import Floor from "@/pages/Home/Floor"
+import Brand from "@/pages/Home/Brand" 
+export default {
+  components:{
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Floor,
+    Brand
+  }
+};
 </script>
 
 <style lang="less" scoped></style>
