facdb06560a3680419eb2003e42892417b26c3ac
asurafe <onelana@163.com>
2023-02-03 16:50:55 +0800

    feat:分页器组件

--- src/main.js
+++ src/main.js
@@ -9,11 +9,14 @@ import 'swiper/css/swiper.css'
 
 // 注册三级联动全局组件
 import TypeNav from "@/components/TypeNav"
+// 注册轮播图全局组件
 import Carousel from "@/components/Carousel"
+// 注册分页器全局组件
+import Pagination from "@/components/Pagination"
 
 Vue.component(TypeNav.name, TypeNav)
 Vue.component(Carousel.name, Carousel)
-
+Vue.component(Pagination.name, Pagination)
 new Vue({
   render: h => h(App),
   // 全局事件总线
